022b1e00e359a7abb97ea6c67416d0d9fccc683f
Mickael <polmickael3@gmail.comgit config --global user.name>
2017-08-08 16:57:28 +0200

    push again for pushing is fun

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ Fonction bonjour( prenom, nom)
     Faire une fonction que vous appelerez bonjour
     Qui prend en paramètre le prénom et le nom
     Qui affiche une alert() avec comme texte par exemple: "Bonjour Prénom Nom comment allez vous?"
-*/
+    */
 
 // function Bonjour(prénom, nom){
 // 	alert("Bonjour" + " " + prénom + " "+  nom + " " + "comment allez vous ?");
@@ -25,7 +25,7 @@ Fonction compteToNombre( nombre )
     qui prends en parametre nombre
     qui va compter de 1 à nombre et l'afficher dans la console ( utiliser une boucle for)
     faire la mème chose en définissant une valeur par défaut à nombre et tester
-*/
+    */
 
 // function compteToNombre(nb){
 // 	for(var i = 1; i <= nb ; i++){
@@ -45,7 +45,7 @@ Fonction operation( param1, param2 )
     Avant la fonction operation faite deux fonctions une addition qui retourne le résultat de l'addition et l'autre multiplication qui retourne le résultat de la multiplication ( return )
     Faite la fonction operation qui va demander à l'utilisateur de saisir deux valeurs ( prompt ) puis se servir des deux fonctions précédente pour afficher l'addition et la multiplication dans une alert.
     Rajouter la division mais pour la division n'appelez la fonction que si le param2 est supérieur à 0 ( car on ne peut pas diviser par 0), mettez un message explicatif
-*/
+    */
 
 // var val1;
 // var val2;
@@ -77,10 +77,15 @@ multiplication(val1,val2);
 
 
 function division(val1,val2){
-	var resultat;
+	var result; 
 	resultat = val1/val2;
 	return resultat;
-	
+
+	// je fait ma division 
+	// SI je divisie par 0 ALORS
+	// renvoi "tune peux pas diviser par 0"
+	// SINON 
+	// jexecute la division
 };
 
 division(val1,val2);
@@ -91,10 +96,14 @@ function operation(){
 	val1 = Number(prompt("entrez un chiffre"));
 	val2 = Number(prompt("entrez en un second"));
 	alert("le resultat de l'addition est de : " + " " + addition(val1, val2));
-	alert("le resultat de l'addition est de : " + " " + multiplication(val1, val2));
-	alert(division(val1, val2))
+	alert("le resultat de la multiplication est de : " + " " + multiplication(val1, val2));
+	if (val2 !== 0){
+		alert("le resultat de votre division est de : " + " " + division(val1, val2));
 
-}
+	}else {
+		alert("elle est ou la poulette 0 ??");
+	}
+};
 
 operation();
 
