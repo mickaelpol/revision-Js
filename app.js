b3dc89044b2e718c595c7b7354debcb329d5fa27
Mickael <polmickael3@gmail.comgit config --global user.name>
2017-08-08 16:12:20 +0200

    push for push

--- app.js
+++ app.js
@@ -27,14 +27,81 @@ Fonction compteToNombre( nombre )
     faire la mème chose en définissant une valeur par défaut à nombre et tester
 */
 
-function compteToNombre(nb){
-	for(var i = 1; i <= nb ; i++){
+// function compteToNombre(nb){
+// 	for(var i = 1; i <= nb ; i++){
 
-		console.log(i)
-	}
+// 		console.log(i)
+// 	}
+// }
+
+// compteToNombre(parseInt(prompt("entrez une valeur")));
+
+/* ------------------------------------------------------------------------------*/
+
+
+/*
+Fonction operation( param1, param2 )
+
+    Avant la fonction operation faite deux fonctions une addition qui retourne le résultat de l'addition et l'autre multiplication qui retourne le résultat de la multiplication ( return )
+    Faite la fonction operation qui va demander à l'utilisateur de saisir deux valeurs ( prompt ) puis se servir des deux fonctions précédente pour afficher l'addition et la multiplication dans une alert.
+    Rajouter la division mais pour la division n'appelez la fonction que si le param2 est supérieur à 0 ( car on ne peut pas diviser par 0), mettez un message explicatif
+*/
+
+// var val1;
+// var val2;
+var val1;
+var val2; 
+
+
+function addition(val1, val2){
+	var resultat;
+	resultat = val1+val2;
+	return resultat;
+};
+addition(val1,val2);
+
+
+
+
+
+
+
+function multiplication(val1, val2){
+	var resultat;
+	resultat = val1*val2;
+	return resultat;
+};
+
+multiplication(val1,val2);
+
+
+
+function division(val1,val2){
+	var resultat;
+	resultat = val1/val2;
+	return resultat;
+	
+};
+
+division(val1,val2);
+
+
+
+function operation(){
+	val1 = Number(prompt("entrez un chiffre"));
+	val2 = Number(prompt("entrez en un second"));
+	alert("le resultat de l'addition est de : " + " " + addition(val1, val2));
+	alert("le resultat de l'addition est de : " + " " + multiplication(val1, val2));
+	alert(division(val1, val2))
+
 }
 
-compteToNombre(parseInt(prompt("entrez une valeur")));
+operation();
+
+
+
+/* ----------------------------------------------------------------------------*/
+
 
 
 
